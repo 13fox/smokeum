3980657815614fe17bf337500d3743ca89b0bdfa
13Fox <foxoedov@gmail.com>
2017-11-10 12:21:57 +0300

    10.11-12:21

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,12 +3,18 @@
 
   $(document).ready(function () {
 
-      $(window).focus(function(){
+    /*  $(window).focus(function(){
           setTimeout("location.reload()",200);
           console.log('reload');
-      });
+      });*/
 
+   $('.product').hover(function(){
+    $(this).addClass('hoverproduct');
+   },function(){
 
+    $(this).removeClass('hoverproduct');
+       }
+   );
 
 
 
